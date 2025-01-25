b104bcbf312232270a12d2ea9d97b755c6aa33c2
Roman Abramovich <romanabramovich@rogers.com>
2025-01-25 13:03:02 -0500

    Scraper function returning raw ProtoBuf data from GTFS-RT URL

---
+++ scraper.py
@@ -0,0 +1,16 @@
+from google.transit import gtfs_realtime_pb2
+from config import vehiclesURL
+import requests
+
+#Parse through GTFS-RT TTC vehicles ProtoBuf data
+def scrape_gtfs_rt(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        #Return Raw Protobuf
+        return response.content
+    else: 
+        print(f"Error fetching data: {response.status_code}")
+        return None
+
+
+#NEXT STEP : AUTOMATE ON A TIMER
